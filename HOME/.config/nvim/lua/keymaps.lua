5a607c489a03b597052d2f0a6d5474be33ea80b5
Aleksej Chaichan <aleksej.chaichan@dkb.de>
2025-02-25 11:11:47 +0100

    update

--- HOME/.config/nvim/lua/keymaps.lua
+++ HOME/.config/nvim/lua/keymaps.lua
@@ -33,10 +33,10 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 -- paste in insertmode
 
 -- resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+-- keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+-- keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+-- keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+-- keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- navigate buffers
 keymap("n", "<TAB>", ":bnext<CR>", opts)
