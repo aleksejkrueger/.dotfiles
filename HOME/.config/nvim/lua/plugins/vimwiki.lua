7b03e4fce41a3fe0ad7886195e87cc1ed049f91a
Aleksej Chaichan <aleksej.chaichan@dkb.de>
2025-05-21 16:04:40 +0200

    update

--- HOME/.config/nvim/lua/plugins/vimwiki.lua
+++ HOME/.config/nvim/lua/plugins/vimwiki.lua
@@ -1,5 +1,13 @@
-vim.g.vimwiki_list = {{
-  path = '~/notes/',
-  syntax = 'markdown',
-  ext = '.md'
-}}
+vim.g.vimwiki_global_ext = 0  -- Prevents automatic filetype detection
+vim.g.vimwiki_list = {
+  {
+    path = '~/notes/vimwiki/',
+    syntax = 'markdown',
+    ext = '.md'
+  },
+  {
+    path = '~/notes/',
+    syntax = 'markdown',
+    ext = '.md'
+  }
+}
